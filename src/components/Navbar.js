c5b33367e58b111ee9040d28143af710aa89e244
Jashandeep Singh <jashandeep1659@gmail.com>
2023-02-21 16:38:20 +0530

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+const Navbar = () => {
+    const [NavbarStatus, setNavbarStatus] = useState(false);
+    return (
+        <section id="Navbar">
+            <div className="main-bar">
+                <div className="logo">
+                    <img src="/logo.png" alt="logo" />
+                </div>
+                <button onClick={() => setNavbarStatus(!NavbarStatus)}>
+                    {NavbarStatus ? (
+                        <i className="bx bx-x text-2xl"></i>
+                    ) : (
+                        <i className="bx bx-menu text-2xl"></i>
+                    )}
+                </button>
+            </div>
+            <div className={`mobile-menu ${NavbarStatus ? "active" : null}`}>
+                <ul>
+                    <li className="active link">Home</li>
+                    <li className="link">About Us</li>
+                    <li className="link">Contact Us</li>
+                    <li className="text-red-500 flex items-center  gap-2">
+                        Report fraud{" "}
+                        <i className="bx bx-info-circle bx-rotate-180"></i>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+};
+
+export default Navbar;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+                    rel="stylesheet"
+                ></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
