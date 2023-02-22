095879b78c67ae574c58121ff621da2f51c976c3
Jashandeep Singh <jashandeep1659@gmail.com>
2023-02-22 16:57:50 +0530

    first

---
+++ src/components/whatsapp.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Whatsapp = () => {
+    return (
+        <section id="whatsapp">
+            <Link href="#">
+                <img src="https://cdn.worldvectorlogo.com/logos/whatsapp-3.svg" />
+            </Link>
+        </section>
+    );
+};
+
+export default Whatsapp;
