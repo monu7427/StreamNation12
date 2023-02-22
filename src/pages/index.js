5785da718d4e818ab5bf360737691f4996275dcd
Jashandeep Singh <jashandeep1659@gmail.com>
2023-02-22 20:19:52 +0530

    apis and desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import axios from "axios";
 import AllPlatforms from "@/components/AllPlatforms";
 import Link from "next/link";
 import MainSwiper from "@/components/MainSwiper";
-import axios from "axios";
 
 const Home = (props) => {
     const [NavbarHeight, setNavbarHeight] = useState(0);
